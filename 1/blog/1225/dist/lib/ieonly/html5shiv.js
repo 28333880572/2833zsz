617be4a3c60e396e08edec5d859723069a8737a8
233333 <48607307+28333880572@users.noreply.github.com>
2019-10-19 20:31:37 +0800

    同富裕

---
+++ 1/blog/1225/dist/lib/ieonly/html5shiv.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 - 页面已走丢，请返回首页</title>
+    <link rel="shortcut icon"  type="image/x-icon" href="favicon.png">
+    <style type="text/css">
+		.box{
+            text-align: center;
+            color: #999;
+            font-size: 15px;
+            padding-top: 50px;
+        }
+        .link a{
+            display: inline-block;
+            color: #fff;
+            text-decoration: none;
+            font-size: 16px;
+            margin-top: 20px;
+            padding: 10px 25px;
+            background: #fe925c;
+            border-radius: 5px;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="box">
+        <img src="https://webjike.com/images/404.png">
+        <div class="link">
+            <p>你访问的页面不存在，文件可能已被删除或路径发生变化</p>
+            <p>小呆导航 V3.0 · www.webjike.com</p>
+            <p><a href="/index.html">返回首页</a></p>
+        </div>
+    </div>
+  </body>
+</html>
