d06d5b7f4b085d1aade94868c0d43c568e081f85
233333 <48607307+28333880572@users.noreply.github.com>
2021-02-25 14:26:39 +0800

    ..

--- muisc/copy.163music-master/src/store/mutation-types.js
+++
@@ -1,8 +0,0 @@
-export const TOGGLE_PLAY = 'TOGGLE_PLAY'
-export const PLAY = 'PLAY'
-export const STOP = 'STOP'
-export const SHOW = 'SHOW'
-export const HIDE = 'HIDE'
-export const SHOW_MENU = 'SHOW_MENU'
-export const HIDE_MENU = 'HIDE_MENU'
-export const TOGGLE_MENU = 'TOGGLE_MENU'
